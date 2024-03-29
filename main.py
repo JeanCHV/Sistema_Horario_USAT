2ce9cf2538bcc4b5a2370914a89aa05c34d0b0f4
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2024-03-28 22:41:40 -0500

    Commit inicial de creacion de entorno Flask

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/login")
+def login():
+    return render_template("login/login.html")
+@app.route("/index")
+def index():
+    return render_template("dashboard/index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
