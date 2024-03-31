40211ce77bb34cb757049233404462e918e3179f
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2024-03-31 00:39:13 -0500

    Cambios en rutas

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
 from flask import Flask, render_template, request, redirect, jsonify
 app = Flask(__name__)
-
-@app.route("/")
-@app.route("/login")
-def login():
-    return render_template("login/login.html")
-
-
-@app.route("/index")
-def index():
-    return render_template("dashboard/home.html")
-
-@app.route("/ambientes")
-def ambientes():
-    return render_template("dashboard/ambientes.html")
-
-@app.route("/cursos")
-def cursos():
-    return render_template("dashboard/cursos.html")
-
-
-@app.route("/docentes")
-def docentes():
-    return render_template("dashboard/docentes.html")
-
-@app.route("/horarios")
-def horarios():
-    return render_template("dashboard/horarios.html")
+from routers.router_main import *
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
