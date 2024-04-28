1b30ca27e40fc241ad5318018e4480d710e7b669
DavDCV <caballerovasquezdavidantonio@gmail.com>
2024-04-28 18:00:47 -0500

    Caballero

---
+++ controladores/usuario/controlador_persona.py
@@ -0,0 +1,30 @@
+from bd import obtenerConexion
+
+def obtener_personas():
+    conexion = obtenerConexion()
+    personas = []
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT idpersona,nombres,apellidos,n_documento,telefono,correo,tipopersona,cantHoras,tiempo_ref,foto FROM persona")
+        personas = cursor.fetchall()
+    conexion.close()
+    return personas
+
+def obtener_persona_por_id(id):
+    conexion = obtenerConexion()
+    persona = None
+    with conexion.cursor() as cursor:
+        cursor.execute(
+            "SELECT idpersona,nombres,apellidos,n_documento,telefono,correo,tipopersona,cantHoras,tiempo_ref,foto FROM persona WHERE idpersona = %s", (id,))
+        persona = cursor.fetchone()
+    conexion.close()
+    return persona
+
+def obtener_persona_por_dni(dni):
+    conexion = obtenerConexion()
+    persona = None
+    with conexion.cursor() as cursor:
+        cursor.execute(
+            "SELECT idpersona,nombres,apellidos,n_documento,telefono,correo,tipopersona,cantHoras,tiempo_ref,foto FROM persona WHERE idpersona = %s", (dni,))
+        persona = cursor.fetchone()
+    conexion.close()
+    return persona
