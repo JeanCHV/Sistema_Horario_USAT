70ad62ff8f9a73fd46053e6a5f40eb385b700f31
DavDCV <caballerovasquezdavidantonio@gmail.com>
2024-05-03 18:27:15 -0500

    Caballero

--- bd.py
+++ bd.py
@@ -1,16 +1,16 @@
 import pymysql
 
 
-def obtener_conexion():
+""" def obtener_conexion():
     return pymysql.connect(host='sql3.freemysqlhosting.net',
                                 port=3306,
                                 user='sql3702269',
                                 password='j1pBlTp3hP',
-                                db='sql3702269')
+                                db='sql3702269') """
 
-""" def obtener_conexion():
+def obtener_conexion():
     return pymysql.connect(host='127.0.0.1',
                                 port=3306,
                                 user='root',
                                 password='',
-                                db='db_calidad') """
+                                db='sis_horarios_usat')
