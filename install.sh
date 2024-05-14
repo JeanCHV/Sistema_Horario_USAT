a10481282e1646093f41ec307c8bf12b4769d2f7
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2024-05-14 08:22:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,14 @@
 # Crea un entorno virtual
 py -3 -m venv .venv
 
+# Espera un segundo
+sleep 1
+
 # Activa el entorno virtual
 source .venv/bin/activate
+
+# Espera un segundo
+sleep 1
 
 # Instala los requisitos del archivo requirements.txt
 pip install -r requirements.txt
