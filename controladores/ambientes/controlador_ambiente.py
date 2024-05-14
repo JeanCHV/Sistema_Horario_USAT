12e409a4607385430402deae03da858c5f51f8c0
BrunoPisfil <brunosp1100@gmail.com>
2024-05-14 09:53:02 -0500

    Agregados v3 Ambiente

--- controladores/ambientes/controlador_ambiente.py
+++ controladores/ambientes/controlador_ambiente.py
@@ -29,43 +29,30 @@ def agregar_ambiente(nombre, aforo, estado, idedificio, idambientetipo):
         return {"error": str(e)}
     finally:
         conexion.close()
+        
 
-
-
-# def obtener_usuario_con_tipopersona_por_username(username):
-#     conexion = obtener_conexion()
-#     usuario = None
-#     with conexion.cursor() as cursor:
-#         cursor.execute(
-#             "SELECT usu.idusuario, usu.username, usu.password, usu.estado, usu.idpersona, usu.token, per.tipopersona FROM usuario usu INNER JOIN persona per ON per.idpersona = usu.idpersona WHERE usu.username =  %s", (username,))
-#         usuario = cursor.fetchone()
-#     conexion.close()
-#     return usuario
-
-# def obtener_usuario_por_username(username):
-#     conexion = obtener_conexion()
-#     usuario = None
-#     with conexion.cursor() as cursor:
-#         cursor.execute(
-#             "SELECT idusuario, username, password, estado, idpersona, token FROM usuario WHERE username = %s", (username,))
-#         usuario = cursor.fetchone()
-#     conexion.close()
-#     return usuario
-
-# def obtener_usuario_por_id(id):
-#     conexion = obtener_conexion()
-#     usuario = None
-#     with conexion.cursor() as cursor:
-#         cursor.execute(
-#             "SELECT idusuario, username, password, estado, idpersona, token FROM usuario WHERE id = %s", (id,))
-#         usuario = cursor.fetchone()
-#     conexion.close()
-#     return usuario
-
-# def actualizar_token(username,token):
-#     conexion = obtener_conexion()
-#     with conexion.cursor() as cursor:
-#         cursor.execute("UPDATE usuario SET token = %s WHERE username = %s",
-#                        (token,username))
-#     conexion.commit()
-#     conexion.close()
+def eliminar_ambiente(idambiente):
+    conexion = obtener_conexion()
+    try:
+        with conexion.cursor() as cursor:
+            cursor.execute("delete from ambiente where idambiente = %s)",
+                           (idambiente))
+            conexion.commit()
+            return {"mensaje": "Ambiente eliminado correctamente"}
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        conexion.close()
+        
+def modificar_ambiente(nombre, aforo, estado, idedificio, idambientetipo):
+    conexion = obtener_conexion()
+    try:
+        with conexion.cursor() as cursor:
+            cursor.execute("update from ambiente (nombre, aforo, estado, idedificio, idambientetipo) VALUES (%s, %s, %s, %s, %s))",
+                           (nombre, aforo, estado, idedificio, idambientetipo))
+            conexion.commit()
+            return {"mensaje": "Ambiente modificado correctamente"}
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        conexion.close()
