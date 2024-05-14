d16a65e42bbca240ac46274d2068bde254840378
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2024-05-13 20:34:31 -0500

    updte

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-app.debug = True
+app.debug = False
 app.secret_key = 'super-secret'
 from routers.router_main import *
 
 if __name__ == "__main__":
-    app.run( debug=True)
+    app.run( debug=False)
