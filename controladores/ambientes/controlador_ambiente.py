807e3e990d8a8f0165883a6eee0bc657e1074ca7
BrunoPisfil <brunosp1100@gmail.com>
2024-05-23 09:48:51 -0500

    AGREGADOS_AMBIENTE_23_MAYO

--- controladores/ambientes/controlador_ambiente.py
+++ controladores/ambientes/controlador_ambiente.py
@@ -1,17 +1,16 @@
 from bd import obtener_conexion
-from flask import request, jsonify
 
 def obtener_ambientes():
     conexion = obtener_conexion()
     ambientes = []
 
     with conexion.cursor() as cursor:
-        cursor.execute("SELECT idambiente, nombre, aforo, estado, idedificio, idambientetipo FROM ambiente")
-        column_names = [desc[0] for desc in cursor.description]  # Obtener los nombres de las columnas
+        cursor.callproc('sp_Ambiente_Gestion', [0, None, None, None, None, None, None])
+        column_names = [desc[0] for desc in cursor.description]
         rows = cursor.fetchall()
 
         for row in rows:
-            ambiente_dict = dict(zip(column_names, row))  # Convertir cada fila en un diccionario
+            ambiente_dict = dict(zip(column_names, row))
             ambientes.append(ambiente_dict)
 
     conexion.close()
@@ -21,8 +20,7 @@ def agregar_ambiente(nombre, aforo, estado, idedificio, idambientetipo):
     conexion = obtener_conexion()
     try:
         with conexion.cursor() as cursor:
-            cursor.execute("INSERT INTO ambiente (nombre, aforo, estado, idedificio, idambientetipo) VALUES (%s, %s, %s, %s, %s)",
-                           (nombre, aforo, estado, idedificio, idambientetipo))
+            cursor.callproc('sp_Ambiente_Gestion', [1, None, nombre, aforo, estado, idedificio, idambientetipo])
             conexion.commit()
             return {"mensaje": "Ambiente agregado correctamente"}
     except Exception as e:
@@ -30,6 +28,17 @@ def agregar_ambiente(nombre, aforo, estado, idedificio, idambientetipo):
     finally:
         conexion.close()
 
+def modificar_ambiente(idambiente, nombre, aforo, estado, idedificio, idambientetipo):
+    conexion = obtener_conexion()
+    try:
+        with conexion.cursor() as cursor:
+            cursor.callproc('sp_Ambiente_Gestion', [2, idambiente, nombre, aforo, estado, idedificio, idambientetipo])
+            conexion.commit()
+            return {"mensaje": "Ambiente modificado correctamente"}
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        conexion.close()
 
 def eliminar_ambiente(idambiente):
     conexion = obtener_conexion()
@@ -50,18 +59,6 @@ def dar_baja_ambiente(idambiente):
             cursor.callproc('sp_Ambiente_Gestion', [3, idambiente, None, None, None, None, None])
             conexion.commit()
             return {"mensaje": "Ambiente dado de baja correctamente"}
-    except Exception as e:
-        return {"error": str(e)}
-    finally:
-        conexion.close()
-
-def modificar_ambiente(idambiente, nombre, aforo, estado, idedificio, idambientetipo):
-    conexion = obtener_conexion()
-    try:
-        with conexion.cursor() as cursor:
-            cursor.callproc('sp_Ambiente_Gestion', [2, idambiente, nombre, aforo, estado, idedificio, idambientetipo])
-            conexion.commit()
-            return {"mensaje": "Ambiente modificado correctamente"}
     except Exception as e:
         return {"error": str(e)}
     finally:
