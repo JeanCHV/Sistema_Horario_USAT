d00289d9114a20e50c30088fa1c096972a87bc08
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2024-06-18 07:39:39 -0500

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,12 +1,12 @@
 import pymysql
 
 #Desarrollo
-# def obtener_conexion():
-#     return pymysql.connect(host='roundhouse.proxy.rlwy.net',
-#                                 port=54363,
-#                                 user='root',
-#                                 password='UAumJRVIMWnTGqRdkZNDqvocCpHjzHKl',
-#                                 db='db_calidad')
+def obtener_conexion():
+    return pymysql.connect(host='roundhouse.proxy.rlwy.net',
+                                port=54363,
+                                user='root',
+                                password='UAumJRVIMWnTGqRdkZNDqvocCpHjzHKl',
+                                db='db_calidad')
 
 
 # def obtener_conexion():
@@ -16,9 +16,9 @@ import pymysql
 #                                 password='BwWVzLlZ2IEbvqPx33cy',
 #                                 db='bkvjs2sg6xirjg6gaoca')
 
-def obtener_conexion():
-    return pymysql.connect(host='127.0.0.1',
-                                port=3306,
-                                user='root',
-                                password='',
-                                db='db_calidad')
+# def obtener_conexion():
+#     return pymysql.connect(host='127.0.0.1',
+#                                 port=3306,
+#                                 user='root',
+#                                 password='',
+#                                 db='db_calidad')
