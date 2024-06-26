d8d17eae955da8bfc2b2424b6bbc17acd4460ae0
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2024-06-25 20:00:36 -0500

    Update router_page_not_found.py

--- routers/router_page_not_found.py
+++ routers/router_page_not_found.py
@@ -4,6 +4,5 @@ from flask import request, session, redirect, url_for,render_template
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # Aquí puedes redirigir al usuario al index.html o renderizar directamente esa plantilla
     return redirect(url_for('index'))
 
